3c7ff0b08683a8fbdefb1a7e0a2ada347329a01c
Truong Tran Vu <tranvutruongs2@gmail.com>
2020-07-30 13:16:19 +0700

    Truong commit
    Booking

--- ProjectCSharpCGV/View/Booking/BookingSeat.aspx.designer.cs
+++ ProjectCSharpCGV/View/Booking/BookingSeat.aspx.designer.cs
@@ -22,5 +22,14 @@ namespace ProjectCSharpCGV.View.Booking
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// rptView control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptView;
     }
 }
