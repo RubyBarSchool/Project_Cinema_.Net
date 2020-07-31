e2e6842249e61cd9426e1328bc2be3f1036cdf56
Truong Tran Vu <tranvutruongs2@gmail.com>
2020-07-31 12:41:27 +0700

    Truong commit v9
    Truong commit

--- ProjectCSharpCGV/App_Code/SlotsShowDAO.cs
+++ ProjectCSharpCGV/App_Code/SlotsShowDAO.cs
@@ -15,7 +15,7 @@ namespace ProjectCSharpCGV.App_Code
             string sql = "SELECT idRap,idPhong,idFilms,showDate,booked,statusClose,idSlots FROM dbo.SlotShow";
             List<SlotShow> list = new List<SlotShow>();
             DataTable dt = DataAccess.ReadDataBySQL(sql);
-            foreach(DataRow dr in dt.Rows)
+            foreach (DataRow dr in dt.Rows)
             {
                 SlotShow ss = new SlotShow();
                 int idRap = Convert.ToInt32(dr["idRap"].ToString());
@@ -39,7 +39,7 @@ namespace ProjectCSharpCGV.App_Code
                 new SqlParameter("@id",SqlDbType.Int)
             };
             param[0].Value = idFilm;
-            DataTable dt = DataAccess.ReadDataBySQLWithParameter(sql,param);
+            DataTable dt = DataAccess.ReadDataBySQLWithParameter(sql, param);
             foreach (DataRow dr in dt.Rows)
             {
                 SlotShow ss = new SlotShow();
@@ -55,6 +55,57 @@ namespace ProjectCSharpCGV.App_Code
                 list.Add(ss);
             }
             return list;
+        }
+        public static SlotShow getAllSlotShowById(int idRapx, int idRoomx, int idFilmx, int idSlotx)
+        {
+            string sql = "SELECT idRap,idPhong,idFilms,showDate,booked,statusClose,idSlots FROM dbo.SlotShow " +
+                "WHERE idFilms = @idFilm AND idRap = @idRap AND idPhong = @idRoom AND idSlots = @idSlots";
+            SqlParameter[] param = new SqlParameter[]{
+                new SqlParameter("@idFilm",SqlDbType.Int),
+                new SqlParameter("@idRap",SqlDbType.Int),
+                new SqlParameter("@idRoom",SqlDbType.Int),
+                new SqlParameter("@idSlots",SqlDbType.Int)
+            };
+            param[0].Value = idFilmx;
+            param[1].Value = idRapx;
+            param[2].Value = idRoomx;
+            param[3].Value = idSlotx;
+            DataTable dt = DataAccess.ReadDataBySQLWithParameter(sql, param);
+            SlotShow ss = new SlotShow();
+            foreach (DataRow dr in dt.Rows)
+            {
+                int idRap = Convert.ToInt32(dr["idRap"].ToString());
+                int idPhong = Convert.ToInt32(dr["idPhong"].ToString());
+                ss.Theart_Roomx = Theart_RoomDAO.getAllRapPhongByID(idRap, idPhong);
+                ss.Filmsx = FilmsDAO.getDetailFilm(Convert.ToInt32(dr["idFilms"].ToString()));
+                ss.ShowDate = Convert.ToDateTime(dr["showDate"].ToString());
+                ss.Booked = dr["booked"].ToString();
+                ss.StatusClose = Convert.ToBoolean(dr["statusClose"].ToString());
+                int idSlots = Convert.ToInt32(dr["idSlots"].ToString());
+                ss.Slotsx = SlotsDAO.getSlotsByID(idSlots);
+            }
+            return ss;
+        }
+        public static bool updateSlotsShow(SlotShow ssshow)
+        {
+            string sql = "UPDATE [dbo].[SlotShow]   SET      [booked] = @book  " +
+                "WHERE idRap = @idRap AND idPhong = @idRoom AND idFilms = @idFilm AND idSlots = @slot AND showDate = @showDate ";
+
+            SqlParameter[] param = new SqlParameter[]{
+                new SqlParameter("@book",SqlDbType.NVarChar),
+                new SqlParameter("@idRap",SqlDbType.Int),
+                new SqlParameter("@idRoom",SqlDbType.Int),
+                new SqlParameter("@idFilm",SqlDbType.Int),
+                new SqlParameter("@slot",SqlDbType.Int),
+                new SqlParameter("@showDate",SqlDbType.DateTime)
+            };
+            param[0].Value = ssshow.Booked;
+            param[1].Value = ssshow.Theart_Roomx.Theartx.ID;
+            param[2].Value = ssshow.Theart_Roomx.Roomx.ID;
+            param[3].Value = ssshow.Filmsx.ID;
+            param[4].Value = ssshow.Slotsx.ID;
+            param[5].Value = ssshow.ShowDate;
+            return DataAccess.CUDDataBySQL(sql, param);
         }
     }
 }
